c44f1cf96e5af2ef77edf3f3ecbd554956315710
Phaneendra Marisa <phaneendra.marisa@paysafe.com>
2018-10-23 15:06:46 +0530

    added a simple circuit breaker - netflix hystrix

--- readme.md
+++ readme.md
@@ -3,10 +3,44 @@ eureka - service discovery - meta data - host, port, health indicator url, home 
 
 hystrix - circuit breaker
 
-zuul - intelligent routing
+feign - dynamic rest client
 
 ribbon - client side load balancing
 
+zuul - intelligent routing
 
 
 make a call from one sevice to another
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
--- src/main/java/com/paysafe/op/sample/elasticsearch/controller/GiftCardController.java
+++ src/main/java/com/paysafe/op/sample/elasticsearch/controller/GiftCardController.java
@@ -1,14 +1,21 @@
 package com.paysafe.op.sample.elasticsearch.controller;
 
+import java.net.URI;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.paysafe.op.sample.elasticsearch.model.GiftCard;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Component
+@EnableCircuitBreaker
 public class GiftCardController {
 
     private static final String template = "Hello, %s!";
@@ -19,4 +26,21 @@ public class GiftCardController {
         GiftCard g = new GiftCard(counter.incrementAndGet(), String.format(template, name));
         return g;
     }
+
+    @GetMapping("/dummy")
+    @HystrixCommand(fallbackMethod = "reliable")
+    public String dummy(){
+        //        String tmp_string = "dummy from sample es";
+//        return tmp_string;
+
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = URI.create("http://localhost:8021/v1/dummy");
+
+        return restTemplate.getForObject(uri, String.class);
+    }
+
+    private String reliable() {
+        return "circuit breaker";
+    }
+
 }
